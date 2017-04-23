ac5a2102573adc001a8f85e02bf0e183d0d7a161
Natoya <natoyaabiola@gmail.com>
2017-04-23 15:46:39 -0600

    Add files via upload

---
+++ use-the-tdd-force/code.rb
@@ -0,0 +1,38 @@
+
+def plus_two(number)
+  result = number + 2
+end
+#
+def subtract_or_multiply_by_two(number)
+  if number < 0
+    result = number - 2
+  else
+    result = number * 2
+  end
+end
+
+def repeat_last_word(sentence)
+  result = sentence + (" ") + sentence.split(" ")[-1]
+end
+
+def return_value_of_type(input)
+
+  if input == string
+    String
+  elsif input == fixnum
+    Fixnum
+  else
+    "input does not match any Ruby class"
+  end
+end
+
+
+def raise_standard_error
+  raise StandardError
+end
+
+def add_five_and_ten(array)
+  [5,10].each do |number|
+    array << number
+  end
+end
