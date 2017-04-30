334e20a9c92b87ab22afa3f2c46b955c25010734
Natoya <natoyaabiola@gmail.com>
2017-04-29 18:51:44 -0600

    Add files via upload

---
+++ news-aggregator/app.rb
@@ -0,0 +1,43 @@
+require "sinatra"
+
+
+get "/articles" do
+  @articles = File.readlines("articles.csv")
+  @urls = File.readlines("urls.csv")
+  @descriptions = File.readlines("descriptions.csv")
+  erb :index
+end
+
+get "/articles/new" do
+  article = params[:article]
+  article_url = params[:article_url]
+  article_description = params[:article_description]
+  erb :submissions
+end
+
+get "/articles/:article_url" do
+  article_url = params[:article_url]
+  erb :index
+end
+
+
+post "/articles/new" do
+  article = params[:article]
+  article_url = params[:article_url]
+  article_description = params[:article_description]
+
+
+  File.open("articles.csv", "a") do |file|
+    file.puts(article)
+  end
+
+  File.open("urls.csv", "a") do |file|
+    file.puts(article_url)
+  end
+
+  File.open("descriptions.csv", "a") do |file|
+    file.puts(article_description)
+  end
+
+  redirect "/articles"
+end
