fa1aeedd63094e6b700ba3a29c2fa80af1576403
Natoya <natoyaabiola@gmail.com>
2017-08-03 20:20:02 -0600

    Add files via upload

---
+++ startup-mad-libs/main.js
@@ -0,0 +1,9 @@
+let companyName = 'ZenKare';
+let anOffering = ' is an e-health platform that offers';
+let aTargetAudience = ' the world';
+let solveAProblem = ' emotional counselling services';
+let secretSauce = ' from anywhere';
+
+//your code, here
+let elevatorPitch = companyName + anOffering +  aTargetAudience + solveAProblem + secretSauce
+console.log (elevatorPitch);
